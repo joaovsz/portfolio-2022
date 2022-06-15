f1cada61601b33a38e4c6c5b4880a4cf0bfa48bc
joaovsz <joaovitor1713coin@gmail.com>
2022-06-15 13:19:22 -0300

    base version

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './global.css'
 import Header from './components/Header/Header'
 import Apresentation from './components/Main/Apresentation';
 import Knowledges from './components/Conhecimentos/Knowledges';
+import Projects from './components/Projetos/Projects';
 function App() {
 
   return (
     <> 
     <Header/>
     <Apresentation/>
+    <Projects/>
     <Knowledges/>
     </>
   )
---
+++ src/components/Card/Badges.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Badges.css"
+const Badges = ({image}) => {
+  return <img id="badges" src={image}></img>
+  
+      
+  
+    
+  
+}
+
+export default Badges
